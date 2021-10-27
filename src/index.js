4a21b88a68da22441f1d6b4d3c459a66d8bc9cb9
Mathias Eudeline <m.eudeline@crown.fr>
2021-10-27 18:17:14 +0200

    Removing vsSelect from now due to issues

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 export default install
 
 export { default as vsButton } from './components/vsButton'
-export { default as vsSelect } from './components/vsSelect'
+//export { default as vsSelect } from './components/vsSelect'
 export { default as vsSwitch } from './components/vsSwitch'
 export { default as vsCheckbox } from './components/vsCheckBox'
 export { default as vsRadio } from './components/vsRadio'
